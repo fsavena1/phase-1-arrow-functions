de9a44506d0f6fa38ff0029251971cc0a75e34e1
frank <franksavena@gmail.com>
2022-10-31 20:19:51 -0500

    commit tree 6154ae7616b53464cd7c51f346b38fefa8cbf04d

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide =  numbers => num2 / num3  
+
+let num2 = 2000
+let num3 = 100
+
+const square = shape => num4 * num4
+
+let num4 = 2
+
+const add = math => num5 + num6
+
+let num5 = 3
+let num6 = 4
